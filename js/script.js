97f1f0fdceab2789f09f7e3e8852d19466b08679
norikodohi <norikodohi28@gmail.com>
2022-02-06 19:37:36 +0900

    見た目の変更

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-$(".openbtn1").click(function () {//ボタンがクリックされたら
+$(".openbtn").click(function () {//ボタンがクリックされたら
     $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-      $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+      $("#sp-gnav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
   });
   
-  $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-      $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
-      $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+  $("#sp-gnav a").click(function () {//ナビゲーションのリンクがクリックされたら
+      $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+      $("#sp-gnav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
   });
